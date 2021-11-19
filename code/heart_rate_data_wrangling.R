ec2d6a90f42266ebde057318cc813e146a0ff958
ornwipa <fahornwipa@gmail.com>
2021-11-18 19:37:20 -0500

    visualize % hrr by work period and harvesting method

--- code/heart_rate_data_wrangling.R
+++ code/heart_rate_data_wrangling.R
@@ -86,4 +86,10 @@ shapiro.test(hrr$pHRR[hrr$Activity=="Platform"]) # W = 0.90023, p-value = 0.0217
 shapiro.test(sqrt(hrr$pHRR[hrr$Activity=="Ground"])) # W = 0.89785, p-value = 0.01938
 shapiro.test(sqrt(hrr$pHRR[hrr$Activity=="Ladder"])) # W = 0.96376, p-value = 0.5184
 shapiro.test(sqrt(hrr$pHRR[hrr$Activity=="Platform"])) # W = 0.92549, p-value = 0.07732
+
+# Visualize data
+ggline(hrr, x = "Work.Period", y = "pHRR", color = "Activity",
+       xlab = "Work Period", ylab = "% HRR",
+       title = "Percent of heart rate reserve through the work shift",
+       add = c("mean_se", "dotplot"), palette = c("#1B9E77", "#D95F02", "#7570B3"))
              
